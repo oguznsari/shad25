3eb74799f654faf3bbe7c0657782809c07ca8b5c
Oğuzhan <osari@natera.com>
2025-07-01 21:47:06 +0300

    new size variant added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 const HomePage = () => {
   return (
     <div className="h-screen flex items-center justify-center">
-      <Button variant="destructive" size="lg" className="rounded-full">Click me</Button>
+      <Button variant="destructive" size="xl" className="rounded-full">Click me</Button>
     </div>
   );
 };
