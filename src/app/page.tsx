4525ae8eea0d352256e7542b8ed113b54e712729
Oğuzhan <osari@natera.com>
2025-07-01 22:14:00 +0300

    Dashboard with Sidebar and Navbar including avatar component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { CirclePlus } from "lucide-react";
-
 const HomePage = () => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button size="xl" className="rounded-full text-custom-color">
-      <CirclePlus />
-        Click me
-      </Button>
+    <div className="">
+
     </div>
   );
 };
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { Moon } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+const Navbar = () => {
+  return (
+    <nav className="p-4 flex items-center justify-between">
+      {/* LEFT */}
+      collapseButton
+      {/* RIGHT */}
+      <div className="flex items-center gap-4">
+        <Link href="/">Dashboard</Link>
+        <Moon />
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
