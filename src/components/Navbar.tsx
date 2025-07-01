a7eb779da7a5de4ac113596fee4d6c983c3dd9ca
Oğuzhan <osari@natera.com>
2025-07-01 22:37:41 +0300

    Dark Mode tutorial

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import Link from "next/link";
-import { LogOut, Moon, Settings, User } from "lucide-react";
+import { LogOut, Moon, Settings, Sun, User } from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import {
   DropdownMenu,
@@ -9,16 +11,44 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
 
 const Navbar = () => {
+  const { setTheme } = useTheme();
+
   return (
     <nav className="p-4 flex items-center justify-between">
       {/* LEFT */}
       collapseButton
       {/* RIGHT */}
       <div className="flex items-center gap-4">
+        {/* DASHBOARD LINK */}
         <Link href="/">Dashboard</Link>
-        <Moon />
+
+        {/* THEME TOGGLE */}
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" size="icon">
+              <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+              <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+              <span className="sr-only">Toggle theme</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem onClick={() => setTheme("light")}>
+              Light
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("dark")}>
+              Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setTheme("system")}>
+              System
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        {/* USER DROPDOWN */}
         <DropdownMenu>
           <DropdownMenuTrigger>
             <Avatar>
@@ -30,16 +60,16 @@ const Navbar = () => {
             <DropdownMenuLabel>My Account</DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuItem>
-                <User className="h-[1.2rem] w-[1.2rem] mr-2" />
-                Profile
+              <User className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Profile
             </DropdownMenuItem>
             <DropdownMenuItem>
-                <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
-                Settings
+              <Settings className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Settings
             </DropdownMenuItem>
             <DropdownMenuItem variant="destructive">
-                <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
-                Logout
+              <LogOut className="h-[1.2rem] w-[1.2rem] mr-2" />
+              Logout
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
