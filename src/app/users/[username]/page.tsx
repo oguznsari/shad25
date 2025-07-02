e7f30a33e82d1064e2c21e40337d8bdb4a4fbc89
Oğuzhan SARI <oguzn.sari@gmail.com>
2025-07-02 19:32:31 +0300

    Single user page design + Breadcrumb

--- src/app/users/[username]/page.tsx
+++ src/app/users/[username]/page.tsx
@@ -1,5 +1,46 @@
+import CardList from "@/components/CardList";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
 const SingleUserPage = () => {
-  return <div>SingleUserPage</div>;
+  return (
+    <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Dashboard</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/users">Users</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>John Doe</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="mt-4 flex flex-col xl:flex-row gap-8">
+        <div className="w-full xl:w-1/3 space-y-6">
+          <div className="bg-primary-foreground p-4 rounded-lg">Badge</div>
+          <div className="bg-primary-foreground p-4 rounded-lg">Info</div>
+          <div className="bg-primary-foreground p-4 rounded-lg">
+            <CardList title="Recent Activity" />
+          </div>
+        </div>
+        <div className="w-full xl:w-2/3 space-y-6">
+          <div className="bg-primary-foreground p-4 rounded-lg">User Cards</div>
+          <div className="bg-primary-foreground p-4 rounded-lg">Chart</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleUserPage;
